c33eb31c28bfed2c62ab5e4c190cb342ac4de059
Ellen Cooper <ellencooper74@gmail.com>
2017-07-22 17:50:41 -0600

    modifies test to account for modal

--- spec/features/default/user_visits_individual_week_page_spec.rb
+++ spec/features/default/user_visits_individual_week_page_spec.rb
@@ -6,11 +6,9 @@ RSpec.feature "User visits individual week page" do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit week_path(week)
-    
     expect(page).to have_content(week.contestants.first.name)
     expect(page).to have_content(week.contestants.last.name)
     expect(page).to have_content("Total Points: 0", count: 6)
-    expect(page).to have_content("Add Points", count: 6)
     expect(page).to have_css("img", count:6)
   end
 end

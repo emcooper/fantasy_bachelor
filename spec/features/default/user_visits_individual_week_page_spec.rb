621c66b07294af7de89a546f8c7c33683b927987
Ellen Cooper <ellencooper74@gmail.com>
2017-07-22 15:19:25 -0600

    adds styling for individual week pages

--- spec/features/default/user_visits_individual_week_page_spec.rb
+++ spec/features/default/user_visits_individual_week_page_spec.rb
@@ -6,7 +6,7 @@ RSpec.feature "User visits individual week page" do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit week_path(week)
-    save_and_open_page
+    
     expect(page).to have_content(week.contestants.first.name)
     expect(page).to have_content(week.contestants.last.name)
     expect(page).to have_content("Total Points: 0", count: 6)

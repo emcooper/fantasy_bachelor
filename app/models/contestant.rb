33dd704d2c8ac6b7023d0dd3a8027b227653b947
Ellen Cooper <ellencooper74@gmail.com>
2017-07-23 14:15:24 -0600

    refactors weekly_points and season_points

--- app/models/contestant.rb
+++ app/models/contestant.rb
@@ -6,13 +6,16 @@ class Contestant < ApplicationRecord
 
   def weekly_points(week_id)
     weekly_plays = WeeklyScore.where("contestant_id = ? AND week_id = ?", self.id, week_id)
-    weekly_plays_joins_plays = weekly_plays.joins(:play)
-    weekly_plays_joins_plays.sum("play_count * point_value")
+    sum_points(weekly_plays)
   end
 
   def season_points
     season_plays = WeeklyScore.where("contestant_id = ?", self.id)
-    season_plays_joins_plays = season_plays.joins(:play)
-    season_plays_joins_plays.sum("play_count * point_value")
+    sum_points(season_plays)
+  end
+
+  def sum_points(plays)
+    join_with_play = plays.joins(:play)
+    join_with_play.sum("play_count * point_value")
   end
 end

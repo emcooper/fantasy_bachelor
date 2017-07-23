eabbdc03aed62153481a744a5849ce8f4599db77
Ellen Cooper <ellencooper74@gmail.com>
2017-07-23 13:07:41 -0600

    refactors create weekly_scores so that 0 play_count records are not created

--- app/controllers/weekly_scores_controller.rb
+++ app/controllers/weekly_scores_controller.rb
@@ -2,14 +2,14 @@ class WeeklyScoresController < ApplicationController
   def create
     contestant_id = weekly_score_params[:contestant_id][0]
     week_id = weekly_score_params[:week_id][0]
-    plays = weekly_score_params[:plays]
+    plays = weekly_score_params[:plays].delete_if {|k,v| v[0] == ""}
     plays.each do |play_id, count|
       WeeklyScore.create( contestant_id: contestant_id,
                           week_id: week_id,
                           play_id: play_id,
                           play_count: count[0])
     end
-    redirect_to "/weeks/#{week_id[0]}"
+    redirect_to "/weeks/#{week_id}"
   end
 
   private
